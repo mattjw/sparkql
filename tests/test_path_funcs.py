c977e15f9a385d444055276763fbe0b2cd81774d
Matt J Williams <mattjw@users.noreply.github.com>
2019-12-17 23:34:46 +0000

    Rename path funcs and add tests. (#20)

--- tests/test_path_funcs.py
+++ tests/test_path_funcs.py
@@ -2,34 +2,73 @@ from sparkql import StringField, StructObject, ArrayField
 from sparkql import path
 
 
-class TestFieldNames:
+class User(StructObject):
+    full_name = StringField()
+
+
+class Article(StructObject):
+    author = User()
+
+
+class Message(StructObject):
+    sender = User()
+    recipients = ArrayField(User())
+
+
+class TestPathSeq:
 
     @staticmethod
     def test_should_return_correct_list_for_nested_schema():
-        # given
-        class User(StructObject):
-            name = StringField()
-
-        class Article(StructObject):
-            author = User()
+        # given (see above)
 
         # when
-        path_field_names = path.field_names(Article.author.name)
+        path_field_names = path.path_seq(Article.author.full_name)
 
         # then
-        assert path_field_names == ["author", "name"]
+        assert path_field_names == ["author", "full_name"]
 
     @staticmethod
     def test_should_return_correct_list_for_array():
-        # given
-        class User(StructObject):
-            full_name = StringField()
-
-        class Article(StructObject):
-            authors = ArrayField(User())
+        # given (see above)
 
         # when
-        path_field_names = path.field_names(Article.authors.ArrayElem.full_name)
+        path_field_names = path.path_seq(Message.recipients.ArrayElem.full_name)
 
         # then
-        assert path_field_names == ["authors", "full_name"]
+        assert path_field_names == ["recipients", "full_name"]
+
+
+class TestPathStr:
+
+    @staticmethod
+    def test_should_return_correct_str_for_nested_schema():
+        # given (see above)
+
+        # when
+        path_field_names = path.path_str(Article.author.full_name)
+
+        # then
+        assert path_field_names == "author.full_name"
+
+    @staticmethod
+    def test_should_return_correct_str_for_array():
+        # given (see above)
+
+        # when
+        path_field_names = path.path_str(Message.recipients.ArrayElem.full_name)
+
+        # then
+        assert path_field_names == "recipients.full_name"
+
+
+class TestPathCol:
+
+    @staticmethod
+    def test_should_return_correct_column_for_nested_schemas():
+        # given (see above)
+
+        # when
+        column = path.path_col(Article.author.full_name)
+
+        # then
+        assert column == ["author", "name"]
