303bc9dd08a0dfa5f67d8d0a4d0e86005b8b01d3
Matt J Williams <matt-williams@cookpad.com>
2019-12-20 00:28:38 +0000

    Reformat tests.

--- tasks/formatting.py
+++ tasks/formatting.py
@@ -2,4 +2,7 @@ from .utils import run, PROJECT_INFO
 
 
 def reformat():
-    run(f"black {PROJECT_INFO.source_directory}")
+    run(
+        f"black {PROJECT_INFO.source_directory} {PROJECT_INFO.tasks_directory} "
+        f"{PROJECT_INFO.tests_directory}"
+    )
