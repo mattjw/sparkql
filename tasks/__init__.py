944665ecd52bb0e314f86779f20891df35e2d126
Matt J Williams <matt-williams@cookpad.com>
2019-12-20 00:20:28 +0000

    Refactor utils.

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,28 +1,6 @@
-from invoke import run as invoke_run
-from invoke import UnexpectedExit, Result
-
+from .utils import run
 
 SRC_DIR = "sparkql"
-
-
-def run(cmd, pty=None, env=None, *args, **kwargs):
-    """
-    invoke's default runner, with some customisation.
-
-    Customisations:
-    - Ensure PYTHONPATH is present and allows package to be imported.
-    - Do not use UnexpectedExit's default message format. stderr & stdout will be output and
-    """
-    if pty is None:
-        pty = True
-    if env is None:
-        env = {"PYTHONPATH": "./"}
-
-    try:
-        invoke_run(cmd, env=env, pty=pty, *args, **kwargs)
-    except UnexpectedExit as err:
-        result: Result = err.result
-        exit(result.return_code)
 
 
 def test():
