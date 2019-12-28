4bec1f682662ad274cea046fdca10783f28f837d
Matt J Williams <matt-williams@cookpad.com>
2019-12-28 13:15:22 +0000

    Remove pyre.

--- tasks/typechecking.py
+++ tasks/typechecking.py
@@ -4,4 +4,3 @@ from .utils import run, PROJECT_INFO
 def typecheck():
     """Typecheck code."""
     run(f"pytype --config .pytype.cfg")
-    # run(f"pyre --source-directory {PROJECT_INFO.source_directory} --search-path $(poetry env info -p)", echo=True)
