39a69b29e68fda4b6ba5c2fa3c156032e51d2822
Matt J Williams <matt-williams@cookpad.com>
2019-12-28 13:44:41 +0000

    Documentation on how to use type hints; general doc improvements; add explicit pylint dep.

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -2,3 +2,10 @@ from .formatting import reformat
 from .testing import test
 from .linting import lint
 from .typechecking import typecheck
+
+
+def verify_all():
+    """Run all checks."""
+    test()
+    lint()
+    typecheck()
