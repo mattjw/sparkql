155a7e30e090b071643f09831c2de45e2b12cc8f
Matt J Williams <matt-williams@cookpad.com>
2020-01-13 00:07:43 +0000

    Better heavy color for CCI.

--- tasks/utils.py
+++ tasks/utils.py
@@ -7,7 +7,7 @@ from termcolor import cprint
 
 def print_heavy(text):
     """Print `text` to terminal with 'heavy' styling."""
-    cprint(text, attrs=["bold"], on_color="on_grey")
+    cprint(text, "blue", attrs=["bold"], on_color="on_grey")
 
 
 def run(cmd, pty=None, env=None, *args, **kwargs):
