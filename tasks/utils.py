acd783ff483a2b4603db2a1030b3405dd0fa86e7
Matt J Williams <mattjw@users.noreply.github.com>
2020-01-13 00:19:56 +0000

    CircleCI publish to pypi on master branch (#36)

--- tasks/utils.py
+++ tasks/utils.py
@@ -2,6 +2,12 @@
 
 from invoke import run as invoke_run
 from invoke import UnexpectedExit, Result
+from termcolor import cprint
+
+
+def print_heavy(text):
+    """Print `text` to terminal with 'heavy' styling."""
+    cprint(text, "cyan", attrs=["bold", "underline", "dark"], on_color=None)
 
 
 def run(cmd, pty=None, env=None, *args, **kwargs):
