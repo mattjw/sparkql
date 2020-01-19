f215d2e59f9f0bee01e4e58ffb04a2c007d4426d
Matt J Williams <matt-williams@cookpad.com>
2020-01-19 21:40:15 +0000

    Extract next version info.

--- tasks/releasing.py
+++ tasks/releasing.py
@@ -1,4 +1,6 @@
 """Tools to support building and publishing a release."""
+import re
+from dataclasses import dataclass
 from typing import Optional
 
 from invoke import Result
@@ -18,13 +20,21 @@ def prepare_release():
     - Creation of a new git tag.
     - Creation of a new commit, implementing the version increment.
     """
-    print(get_next_version())
+    print(get_version_info())
 
 
 #
 # Utils
 
-def get_next_version() -> Optional[str]:
+@dataclass
+class VersionInfo:
+    current_version: str
+    next_version: Optional[str]  # next version, or None if no bump detected
+    next_tag: Optional[str]  # next git tag, or None if no bump detected
+    increment_type: Optional[str]  # increment type (MAJOR, MINOR, PATCH), or None if no bump detected
+
+
+def get_version_info() -> VersionInfo:
     """
     Determine the version number that the next releaes will be, if any.
 
@@ -35,6 +45,20 @@ def get_next_version() -> Optional[str]:
         A string of the form "0.1.1" there is a new release to generate (based on commit messages), or
         `None` if the messages indicate that there is nothing to release.
     """
-    result: Result = run("cz bump --dry-run --files-only", warn=True, hide=True)
+    cz_output = run("cz bump --dry-run --files-only", warn=True, hide=True).stdout
+    print(cz_output)
 
-    print(result.stdout)
+    """bump: version 0.1.1 → 0.1.2
+tag to create: v0.1.2
+increment detected: PATCH"""
+
+    match = re.search(r"""bump: version ([.\d]+) → ([.\d]+)""", cz_output)
+    current_version, next_version = match.groups()
+
+    match = re.search(r"""tag to create: ([.v\d]+)""", cz_output)
+    next_tag = match.group(1)
+
+    match = re.search(r"""increment detected: ([A-Z]+)""", cz_output)
+    increment_type = match.group(1)
+
+    return VersionInfo(current_version, next_version, next_tag, increment_type)
