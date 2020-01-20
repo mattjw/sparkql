6ee84488862758c97935571d226967a706a1718e
Matt J Williams <matt-williams@cookpad.com>
2020-01-20 00:35:44 +0000

    Clarify

--- tasks/releasing.py
+++ tasks/releasing.py
@@ -42,7 +42,7 @@ def prepare_release():
         print(f"Aborting\ngit tag for next release ({next_ver_info.next_tag}) already exists")
         exit(1)
 
-    # Apply version change
+    # Bump version project TOML
     run(f"poetry version {next_ver_info.next_version}")
     update_toml_commitizen_version(next_ver_info.next_version)
 
