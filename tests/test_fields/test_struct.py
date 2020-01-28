0ebf7c622bebb4e2c26de201f2579bc015f473d6
Matt J Williams <matt-williams@cookpad.com>
2020-01-28 22:42:25 +0000

    Simple test for Includes behaviour

--- tests/test_fields/test_struct.py
+++ tests/test_fields/test_struct.py
@@ -1,7 +1,8 @@
 import pytest
+from pyspark.sql.types import StructType, StructField, StringType
 
 from sparkql.exceptions import InvalidStructError
-from sparkql import Struct, String
+from sparkql import Struct, String, schema
 
 
 class TestStruct:
@@ -20,3 +21,29 @@ class TestStruct:
             @property
             def _spark_type_class(self):
                 return None
+
+
+class TestStructIncludes:
+    @staticmethod
+    def test_should_combine_disjoint_includes():
+        # given
+        class AnObject(Struct):
+            field_a = String()
+
+        class AnotherObject(Struct):
+            field_b = String()
+
+        # when
+        class CompositeObject(Struct):
+            class Includes:
+                an_object = AnObject()
+                another_object = AnotherObject()
+            field_c = String()
+        composite_schema = schema(CompositeObject)
+
+        # expect
+        assert composite_schema == StructType([
+            StructField("field_a", StringType()),
+            StructField("field_b", StringType()),
+            StructField("field_c", StringType()),
+        ])
