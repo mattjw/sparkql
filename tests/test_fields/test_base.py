ccabea799779914839c93ed0d010f1647cab5a52
Matt J Williams <matt-williams@cookpad.com>
2020-02-10 22:22:29 +0000

    add test for should_reject_setting_a_set_parent

--- tests/test_fields/test_base.py
+++ tests/test_fields/test_base.py
@@ -1,4 +1,7 @@
-from sparkql import Float
+import pytest
+
+from sparkql.exceptions import FieldParentError
+from sparkql import Float, Struct
 
 
 class TestBaseField:
@@ -6,10 +9,23 @@ class TestBaseField:
     @staticmethod
     def should_give_correct_info_string():
         # given
-        string_field = Float()
+        float_field = Float()
 
         # when
-        info_str = string_field._info()
+        info_str = float_field._info()
 
         # then
         assert info_str == "<Float \n  spark type = FloatType \n  nullable = True \n  name = None <- [None, None] \n  parent = None>"
+
+    @staticmethod
+    def should_reject_setting_a_set_parent():
+        # given
+        struct = Struct()
+        float_field = Float()._replace_parent(struct)
+
+        another_struct = Struct()
+
+        # when, then
+        with pytest.raises(FieldParentError):
+            float_field._replace_parent(another_struct)
+
