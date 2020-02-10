e7cbd4b6e0eb85298afa41c5ecd71da7c7589f76
Matt J Williams <matt-williams@cookpad.com>
2020-02-10 22:09:33 +0000

    Test for atomic spark types

---
+++ tests/test_fields/test_atomic.py
@@ -0,0 +1,25 @@
+from pyspark.sql.types import ByteType, IntegerType, LongType, ShortType, DecimalType, DoubleType, FloatType, \
+    StringType, BinaryType, BooleanType, DateType, TimestampType
+
+from sparkql.fields.atomic import Byte, Integer, Long, Short, Decimal, Double, Float, String, Binary, Boolean, Date, \
+    Timestamp
+
+
+def test_atomics_have_correct_spark_type_classes():
+    expectations = {
+        Byte: ByteType,
+        Integer: IntegerType,
+        Long: LongType,
+        Short: ShortType,
+        Decimal: DecimalType,
+        Double: DoubleType,
+        Float: FloatType,
+        String: StringType,
+        Binary: BinaryType,
+        Boolean: BooleanType,
+        Date: DateType,
+        Timestamp: TimestampType,
+    }
+    for field_class, spark_type_class in expectations.items():
+        field_instance = field_class()
+        assert field_instance._spark_type_class is spark_type_class
