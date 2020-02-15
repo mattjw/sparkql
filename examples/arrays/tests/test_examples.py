80002de975db97d143a8727039d4fb5410a8c844
Matt J Williams <matt-williams@cookpad.com>
2020-02-15 18:46:06 +0000

    Redundant print

--- examples/arrays/tests/test_examples.py
+++ examples/arrays/tests/test_examples.py
@@ -7,7 +7,6 @@ def test_sparkql_stringified_schema():
 
     # when
     generated_schema = pretty_schema(schema(arrays.Article))
-    print(generated_schema)
 
     # then
     assert generated_schema == arrays.prettified_schema.strip()
