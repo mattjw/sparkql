4aa548ce62dcce70dd8c99422a4c7208a28e8d2b
Matt J Williams <mattjw@users.noreply.github.com>
2020-02-27 00:03:54 +0000

    ci: Enforce 100% coverage

--- tasks/testing.py
+++ tasks/testing.py
@@ -4,22 +4,33 @@ from tasks.utils import run, PROJECT_INFO, print_heavy, prepare_reports_dir
 #
 # Commands
 
+MINIMUM_COVERAGE_PERCENTAGE = 100
+
 
 def test():
     print_heavy(f"\n🔍 TESTING 🔎\n")
     prepare_reports_dir()
+    coverage_dat = PROJECT_INFO.reports_directory / ".coverage"
     run(
         f"""
-        export COVERAGE_FILE={PROJECT_INFO.reports_directory / ".coverage"};
-        pytest -vv --cov={PROJECT_INFO.source_directory} -c {PROJECT_INFO.tests_directory}/.pytest.ini"""
+        export COVERAGE_FILE={coverage_dat};
+        pytest -vv --cov={PROJECT_INFO.source_directory} --cov-report="" -c {PROJECT_INFO.tests_directory}/.pytest.ini"""
     )
     run(
         f"""
-        export COVERAGE_FILE={PROJECT_INFO.reports_directory / ".coverage"};
+        export COVERAGE_FILE={coverage_dat};
         poetry run coverage html -d {PROJECT_INFO.reports_directory / "htmlcov"}"""
     )
     run(
         f"""
-            export COVERAGE_FILE={PROJECT_INFO.reports_directory / ".coverage"};
-            poetry run coverage xml -o {PROJECT_INFO.reports_directory / "coverage.xml"}"""
+        export COVERAGE_FILE={coverage_dat};
+        poetry run coverage xml -o {PROJECT_INFO.reports_directory / "coverage.xml"}"""
+    )
+
+    print("Coverage report...")
+    print(f"Note: Coverage below {MINIMUM_COVERAGE_PERCENTAGE}% will be rejected")
+    run(
+        f"""
+        export COVERAGE_FILE={coverage_dat};
+        poetry run coverage report --fail-under={MINIMUM_COVERAGE_PERCENTAGE}"""
     )
