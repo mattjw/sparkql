fbd26f7453a8e33625ed66b3c0af6987d7c2875f
Matt J Williams <matt-williams@cookpad.com>
2020-04-12 15:22:22 +0100

    Test: disjoint

--- tests/unit/test_schema_merger.py
+++ tests/unit/test_schema_merger.py
@@ -1,5 +1,5 @@
 import pytest
-from pyspark.sql.types import StructType
+from pyspark.sql.types import StructType, StructField, StringType
 
 from sparkql import merge_schemas
 
@@ -14,7 +14,20 @@ class MergeSchemas:
                 StructType(),
                 StructType(),
                 id="empty-schemas"
-            )
+            ),
+            pytest.param(
+                StructType([
+                    StructField("a_string", StringType()),
+                ]),
+                StructType([
+                    StructField("another_string", StringType()),
+                ]),
+                StructType([
+                    StructField("a_string", StringType()),
+                    StructField("another_string", StringType()),
+                ]),
+                id="disjoint-schemas"
+            ),
         ]
     )
     def should_successfully_merge_with(schema_a: StructType, schema_b: StructType, expected_schema: StructType):
