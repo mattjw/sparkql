2f8cd19e9b44ab9d2f16c4e7d09e7cf361e5d4a5
Matt J Williams <matt-williams@cookpad.com>
2020-04-12 15:19:24 +0100

    initial test

--- tests/unit/test_schema_merger.py
+++ tests/unit/test_schema_merger.py
@@ -1,3 +1,31 @@
-# must include example from cookpad
-# jose's provenance nightmare
-# combining lists (?)
+import pytest
+from pyspark.sql.types import StructType
+
+from sparkql import merge_schemas
+
+
+class MergeSchemas:
+    @staticmethod
+    @pytest.mark.parametrize(
+        "schema_a, schema_b, expected_schema",
+        [
+            pytest.param(
+                StructType(),
+                StructType(),
+                StructType(),
+                id="empty-schemas"
+            )
+        ]
+    )
+    def should_successfully_merge_with(schema_a: StructType, schema_b: StructType, expected_schema: StructType):
+        # given ^
+
+        # when
+        merged_schema = merge_schemas(schema_a, schema_b)
+
+        # then
+        assert merged_schema.jsonValue() == expected_schema.jsonValue()
+
+        # ...expect distinct objects
+        assert merged_schema is not schema_a
+        assert merged_schema is not schema_b
