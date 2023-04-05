189d00b51f0b2bd610b650883e2ef899e9db4556
Khaled Hammouda <khaled.hammouda@shopify.com>
2023-04-05 11:58:04 -0400

    feat: add support for setting Decimal type precision and scale

--- tests/unit/test_fields/test_atomic.py
+++ tests/unit/test_fields/test_atomic.py
@@ -50,3 +50,17 @@ from sparkql.fields.atomic import (
 def test_atomics_have_correct_spark_type_classes(sparkql_field_class, spark_type_class):
     field_instance = sparkql_field_class()
     assert field_instance._spark_type_class is spark_type_class
+
+
+def test_decimal_precision_and_scale():
+    d = Decimal()
+    assert d._spark_data_type == DecimalType(10, 0)
+
+    d = Decimal(12)
+    assert d._spark_data_type == DecimalType(12, 0)
+
+    d = Decimal(12, 5)
+    assert d._spark_data_type == DecimalType(12, 5)
+
+    d = Decimal(12, 5)
+    assert d._spark_data_type == DecimalType(12, 5)
